f85b9e55471b5d2e1d444dbee5f241eec4a3697d
roman <roman.gadiev@gmail.com>
2021-01-13 11:00:11 +0200

    Updates

---
+++ precache-manifest.9e7108f7278f331b639e512e583b6afc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8166a8cf2dd93092da636cde1c2c55a6",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "8219b2d2e88e7702a918",
+    "url": "/Todolist/static/css/main.237e32d9.chunk.css"
+  },
+  {
+    "revision": "f1710388e2915039ce2c",
+    "url": "/Todolist/static/js/2.8ba80f58.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Todolist/static/js/2.8ba80f58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8219b2d2e88e7702a918",
+    "url": "/Todolist/static/js/main.e80ecd1d.chunk.js"
+  },
+  {
+    "revision": "8c9ba947015c2dd63600",
+    "url": "/Todolist/static/js/runtime-main.e8babc40.js"
+  }
+]);
